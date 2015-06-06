8339b54251cb500ad86d45d01111ea529fea25be
ivan <tymchenkoivan@gmail.com>
2015-06-07 02:49:06 +0300

    bug fixing, again

--- src/main/java/com.company/config/AppConfig.java
+++ src/main/java/com.company/config/AppConfig.java
@@ -1,12 +1,12 @@
 package com.company.config;
 
+import com.company.dao.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
-import com.company.dao.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
